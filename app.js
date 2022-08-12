6026c50f2626c9c558410b66ac9e65b24a788973
Crahold <manuelitrix@gmail.com>
2022-08-11 19:27:48 -0700

    Volviendo a CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var app = express();
+var cors = require('cors');
 var mongoose = require('mongoose');
 
 // conexión con la bd
@@ -11,24 +12,7 @@ db.once("open", () => console.log("Conectado a la Base de Datos"));
 
 //CONFIGURACION DE RUTAS DE SERVIDOR
 app.use(express.json());
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors());
 const cancionesRoutes = require("./routes/canciones_routes");
 app.use("/canciones", cancionesRoutes);
 
